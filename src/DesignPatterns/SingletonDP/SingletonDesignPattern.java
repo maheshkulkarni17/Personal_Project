0ba5315d5931a4700db72872454e55da599056a7
maheshkulkarni <mahesh.kulkarni17@vit.edu>
2024-05-22 02:08:59 +0530

    Added SingletonDesignPattern

--- src/DesignPatterns/SingletonDP/SingletonDesignPattern.java
+++ src/DesignPatterns/SingletonDP/SingletonDesignPattern.java
@@ -1,12 +1,16 @@
 package DesignPatterns.SingletonDP;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class SingletonDesignPattern {
     // static object is created
     private static SingletonDesignPattern singletonDesignPattern;
     //make the constructor private because no one will create the object
     private SingletonDesignPattern()
     {
-
+        if(singletonDesignPattern!=null)
+            throw new RuntimeException("You are trying to break singleton DP");
     }
     // create one static method so that we can call method directly using class name.
     //method will create the object
@@ -46,7 +50,7 @@ public class SingletonDesignPattern {
 
 class Demo
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 //        this is not possible as constructor is private
 //        SingletonDesignPattern singletonDesignPattern1 = new SingletonDesignPattern();
         System.out.println("Hello");
@@ -65,8 +69,29 @@ class Demo
 
 
         /*  there is way to break the singleton pattern
-        * Using the reflection API - This works at runtime
+        * 1] Using the reflection API - This works at runtime
         * This will change the access modifier private to public and this will
-        * break the singleton design patter*/
+        * break the singleton design patter
+        * 2] */
+
+        Constructor<SingletonDesignPattern> constructor = SingletonDesignPattern.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        SingletonDesignPattern singletonDesignPattern2 = constructor.newInstance();
+        System.out.println(
+                singletonDesignPattern2.hashCode()
+        );
+
+        /*using ENUM or private constructor
+        * we can save our Singleton property, at this point of time added the exception in private constructor
+        *
+        * Exception = Exception in thread "main" java.lang.reflect.InvocationTargetException
+	at java.base/jdk.internal.reflect.DirectConstructorHandleAccessor.newInstance(DirectConstructorHandleAccessor.java:74)
+	at java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:502)
+	at java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:486)
+	at DesignPatterns.SingletonDP.Demo.main(SingletonDesignPattern.java:79)
+Caused by: java.lang.RuntimeException: You are trying to break singleton DP
+	at DesignPatterns.SingletonDP.SingletonDesignPattern.<init>(SingletonDesignPattern.java:13)
+	at java.base/jdk.internal.reflect.DirectConstructorHandleAccessor.newInstance(DirectConstructorHandleAccessor.java:62)
+	... 3 more  */
     }
 }
